15d4b95796b89b1a23f1ca02ce98d918d5218f19
Sakshi <sakshikalokar2893@gmail.com>
2026-05-13 17:42:35 +0530

    Enhance PrintName.java with comments and documentation
    Added comments and a multi-line comment block to explain the program.

--- Day-1/PrintName.java
+++ Day-1/PrintName.java
@@ -1,10 +1,25 @@
 package module1;
-class PrintName 
+class PrintName //Class
 {
-    public static void main(String[] args)
+    public static void main(String[] args) // Main Method
   {
 
         System.out.println("Sakshi");
 
     }
 }
+/*
+    Day 1 of Java Practice ☕💻
+
+Program: Print Name in Java
+
+Learning:
+✔ Class
+✔ Main Method
+✔ System.out.println()
+
+Starting my Java journey step by step 🚀
+
+#java #javaprogramming #coding #beginners #100DaysOfCode
+
+*/
